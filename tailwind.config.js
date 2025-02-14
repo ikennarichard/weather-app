463394b31fff2be3ba16dc0405e90c8840d0de86
Ikenna Richard <oguejioforrichard@gmail.com>
2025-02-14 09:49:13 +0100

    chore: update config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,5 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: "class",
   content: [
     "./app/**/*.{js,jsx,ts,tsx}",
     "./components/**/*.{js,jsx,ts,tsx}",
@@ -8,39 +7,7 @@ module.exports = {
   ],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {
-      colors: {
-        primary: {
-          DEFAULT: "var(--color-primary-default)",
-          light: "var(--color-primary-light)",
-        },
-        secondary: {
-          DEFAULT: "var(--color-secondary-default)",
-          light: "var(--color-secondary-light)",
-        },
-        tertiary: {
-          DEFAULT: "var(--color-tertiary-default)",
-          light: "var(--color-tertiary-light)",
-        },
-        accent: {
-          DEFAULT: "var(--color-accent-default)",
-          light: "var(--color-accent-light)",
-        },
-        grey: {
-          DEFAULT: "var(--color-grey-default)",
-        },
-        slate: {
-          DEFAULT: "var(--color-slate-default)",
-        },
-        dark: {
-          DEFAULT: "var(--color-dark-default)",
-        },
-        light: {
-          DEFAULT: "var(--color-light-default)",
-        },
-        overlay: "var(--color-overlay)",
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
