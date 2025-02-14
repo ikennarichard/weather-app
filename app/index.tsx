5cdd73b53267fe69b8cf87422c758d9de9dbfc41
Ikenna Richard <oguejioforrichard@gmail.com>
2025-02-14 09:50:26 +0100

    refactor code

--- app/index.tsx
+++ app/index.tsx
@@ -5,57 +5,127 @@ import {
   View,
   Image,
   Pressable,
-  ScrollView,
   Keyboard,
   TouchableWithoutFeedback,
+  KeyboardAvoidingView,
+  Platform,
 } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { MaterialIcons, Feather } from "@expo/vector-icons";
-import { weatherImages } from "@/constants/data";
+import {
+  MaterialIcons,
+  Feather,
+  FontAwesome,
+  MaterialCommunityIcons,
+} from "@expo/vector-icons";
 import React from "react";
 import { useWeatherContext } from "@/context/WeatherContext";
+import { useEffect } from "react";
+import { LogLevel, OneSignal } from "react-native-onesignal";
+import { weatherColors } from "@/constants/Colors";
+import { weatherIcons } from "@/constants/data";
+import { formatTime } from "@/utils/formatTime";
+import { getDayFromDate } from "@/utils/getDayName";
+import Animated from "react-native-reanimated";
 
 export default function Index() {
-  const { setColorScheme, theme } = useWeatherContext();
-  return (
-    <SafeAreaView className="p-2 flex-col justify-center gap-10">
-      <StatusBar style="auto" />
-      <View className="flex-row items-center justify-around">
-        {/* text input */}
-        <TextInput
-          className="px-3 rounded-3xl h-12 w-[80%] bg-gray-300"
-          placeholder="Enter your location"
-          onPress={Keyboard.dismiss}
-        />
-        <Pressable onPress={() => setColorScheme('dark')}>
-          {theme == 'light' ? (
-            <Feather name="sun" size={24} />
-          ) : (
-            <Feather name="moon" size={24} />
-          )}
-        </Pressable>
-        {/* dark mode */}
-      </View>
+  const {
+    theme,
+    toggleTheme,
+    locations,
+    handleSearchLocations,
+    handleGetWeather,
+    weather,
+  } = useWeatherContext();
+  const { current, location } = weather;
 
-      <View className="flex-col gap-6">
-        <MaterialIcons name="sunny" color="yellow" size={150} />
-        <View>
-          <Text>23&#176;</Text>
-          <Text>Mostly Cloudy</Text>
+  // useEffect(() => {
+  //   // initialize one signal
+  //   OneSignal.Debug.setLogLevel(LogLevel.Verbose);
+  //   OneSignal.initialize("3ce46b73-83b5-4f26-a712-c4a0cdef9554");
+
+  //   // Also need enable notifications to complete OneSignal setup
+  //   OneSignal.Notifications.requestPermission(true);
+  // }, []);
+  return (
+    <SafeAreaView
+      className={`py-6 px-3 flex-col flex-1 gap-12 ${
+        theme === "dark" ? "bg-red-400" : "bg-slate-100"
+      }`}
+    >
+      <KeyboardAvoidingView>
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+          <View className="flex-row items-center justify-around">
+            {/* text input */}
+            <TextInput
+              className="px-6 rounded-3xl h-12 w-[80%] border border-gray-300"
+              placeholder="Enter your location"
+              secureTextEntry={false}
+              onChangeText={handleSearchLocations}
+            />
+            <Pressable
+              onPress={() => toggleTheme(theme === "dark" ? "light" : "dark")}
+            >
+              {theme == "light" ? (
+                <Feather name="moon" size={24} />
+              ) : (
+                <Feather name="sun" size={24} color="white" />
+              )}
+            </Pressable>
+          </View>
+        </TouchableWithoutFeedback>
+
+        {locations?.length > 0 ? (
+          <View className="flex-col gap-3 rounded-xl absolute top-16 left-3 p-3 z-10 bg-gray-200 w-10/12">
+            {locations?.map((item, index) => (
+              <Pressable
+                key={index}
+                className="flex-row gap-3"
+                onPress={() => handleGetWeather(item.name)}
+              >
+                <FontAwesome name="location-arrow" size={16} />
+                <Text
+                  className={` border-gray-300 pb-1 ${
+                    index === locations.length - 1 ? "border-none" : "border-b"
+                  }`}
+                >
+                  {item.name}, {item.country}
+                </Text>
+              </Pressable>
+            ))}
+          </View>
+        ) : null}
+      </KeyboardAvoidingView>
+
+      <View className="flex-col gap-6 items-center relative z-0">
+        <Text className="text-5xl font-semibold">
+          {location?.name}, {location?.country}
+        </Text>
+        <MaterialCommunityIcons
+          name={weatherIcons[current?.condition?.text]}
+          color={weatherColors[current?.condition?.text]}
+          size={150}
+        />
+        <View className="flex-col items-center">
+          <Text className="text-4xl font-semibold">
+            {current?.temp_c}&#176;
+          </Text>
+          <Text>{current?.condition?.text}</Text>
         </View>
 
-        <View className="flex-row justify-around w-full">
-          <View>
+        <View className="flex-row justify-around w-full items-baseline">
+          <View className="flex-col items-center">
             <Feather name="wind" size={20} />
-            <Text>Value</Text>
+            <Text>{current?.wind_kph}km</Text>
           </View>
-          <View>
+          <View className="flex-col items-center">
             <Feather name="droplet" size={20} />
-            <Text>Value</Text>
+            <Text>{current?.humidity}%</Text>
           </View>
-          <View>
-            <Feather name="sun" size={20} />
-            <Text>Value</Text>
+          <View className="flex-col items-center">
+            <Feather name="clock" size={20} />
+            <Text>
+              {location?.localtime ? formatTime(location?.localtime) : 0}
+            </Text>
           </View>
         </View>
       </View>
@@ -65,21 +135,27 @@ export default function Index() {
           <Feather name="calendar" size={24} />
           <Text>Daily Forecast</Text>
         </View>
-        <ScrollView
+        <Animated.ScrollView
           horizontal
-          contentContainerClassName="p-2 flex gap-4 w-full justify-around"
+          contentContainerStyle={{gap: 10}}
+          showsHorizontalScrollIndicator={false}
         >
-          {[1, 2, 3].map((_, index) => (
+          {weather?.forecast?.forecastday?.map((item, index) => (
             <View
               key={index}
-              className="flex-col items-center bg-gray-200 rounded py-2 px-3"
+              className="flex-col items-center rounded-lg p-6 bg-slate-200/50"
             >
-              <MaterialIcons name="sunny" color="yellow" size={50} />
-              <Text>Day</Text>
-              <Text>Degrees</Text>
+              <MaterialCommunityIcons
+                name={weatherIcons[item?.day?.condition?.text]}
+                color={weatherColors[item?.day?.condition?.text]}
+                size={30}
+              />
+              
+              <Text>{item?.date ? getDayFromDate(item?.date) : ''}</Text>
+              <Text>{item?.day?.avgtemp_c}&#176;</Text>
             </View>
           ))}
-        </ScrollView>
+        </Animated.ScrollView>
       </View>
     </SafeAreaView>
   );
