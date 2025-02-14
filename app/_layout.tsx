c80c4df11b2914bc1b108b12deac56bcfe6f6c96
Ikenna Richard <oguejioforrichard@gmail.com>
2025-02-14 09:48:23 +0100

    fix: remove header on screen

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,11 +2,16 @@ import { Stack } from "expo-router";
 import "../global.css";
 import "react-native-reanimated";
 import WeatherProvider from "@/context/WeatherContext";
+import { StatusBar } from "expo-status-bar";
 
 export default function RootLayout() {
   return (
     <WeatherProvider>
-      <Stack />
+      <StatusBar style="auto" />
+      <Stack screenOptions={{
+        headerShown: false
+
+      }}/>
     </WeatherProvider>
   );
 }
