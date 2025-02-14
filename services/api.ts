2797730ddb9b489124f726d8f11208e2c384c26e
Ikenna Richard <oguejioforrichard@gmail.com>
2025-02-14 09:50:39 +0100

    refactor code

--- services/api.ts
+++ services/api.ts
@@ -1,21 +1,24 @@
-import { ForecastProps } from "@/types";
 import { Alert } from "react-native";
 
-export const fetchForeCast = async ({ apiKey, cityName }: ForecastProps) => {
-  try {
-    const response = await fetch(
-      `https://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${cityName}&days=6&aqi=no&alerts=no`
-    );
+export const foreCastEnpoint = (apiKey: string, cityName: string) =>
+  `https://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${cityName}&days=6&aqi=no&alerts=no`;
 
+export const locationEndPoint = (apiKey: string, cityName: string) =>
+  `https://api.weatherapi.com/v1/search.json?key=${apiKey}&q=${cityName}`;
+
+export const getApiService = async (endpoint: string) => {
+  try {
+    const response = await fetch(endpoint);
+    const json = await response.json();
     if (!response.ok) {
       console.error(response);
       throw new Error("Failed to fetch resources");
     }
+    return json;
   } catch (error) {
     if (error instanceof Error) {
       Alert.alert(error.message);
-      console.error(error);
     }
-    throw error;
+    console.error(error);
   }
 };
